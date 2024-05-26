a5b3a569fcc7a61119bcc42179e138f3555c63d3
chris-kali <kwebpr@gmail.com>
2024-05-26 21:29:17 +0200

    création de restaurants avec leur affichage global, leur route et leur affichage unique

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,5 +1,25 @@
 class RestaurantsController < ApplicationController
     def index
-      
+        @restaurants = Restaurant.all
     end
+
+    def show
+        @restaurant = Restaurant.find(params[:id])    
+    end
+
+    def new
+        @restaurant = Restaurant.new # Needed to instantiate the form_with
+    end
+
+    def create
+        @restaurant = Restaurant.new(restaurant_params)
+        @restaurant.save
+        redirect_to restaurants_path(@restaurant)
+      end
+      
+      private
+      
+      def restaurant_params
+        params.require(:restaurant).permit(:nom, :adresse, :note)
+      end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,13 +3,16 @@ Rails.application.routes.draw do
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
-  root to: "pages#home"
+  root "pages#home"
   
   get "ici", to: "pages#about"
   get "hihé", to: "pages#hihi", as: :hihi
   get "restaurants", to: "restaurants#index"
-
+  get "restaurants/new", to: "restaurants#new"
+  post "restaurants", to: "restaurants#create"
+  get "restaurants/:id", to: "restaurants#show"
+  
   get "up" => "rails/health#show", as: :rails_health_check
-  # Defines the root path route ("/")
-  # root "posts#index"
+  
 end
+
