6f3bb954eb2f22101d6921e6fb6731ee51cdb959
chris-kali <kwebpr@gmail.com>
2024-05-28 21:41:20 +0200

    finalisation du CRUD : tout fonctionne

--- config/routes.rb
+++ config/routes.rb
@@ -8,9 +8,12 @@ Rails.application.routes.draw do
   get "ici", to: "pages#about"
   get "hihé", to: "pages#hihi", as: :hihi
   get "restaurants", to: "restaurants#index"
-  get "restaurants/new", to: "restaurants#new"
+  get "restaurants/new", to: "restaurants#new", as: :new_restaurant
   post "restaurants", to: "restaurants#create"
   get "restaurants/:id", to: "restaurants#show", as: :restaurant
+  get "restaurants/:id/edit", to: "restaurants#edit", as: :edit_restaurant
+  patch "restaurants/:id", to: "restaurants#update"
+  delete "restaurants/:id", to: "restaurants#destroy"
   
   get "up" => "rails/health#show", as: :rails_health_check
   
